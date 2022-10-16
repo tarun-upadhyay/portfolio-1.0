9348888b43d329950437ac341bd7c01cbc71e505
tarun-upadhyay <tarunu88@gmail.com>
2022-10-16 20:17:18 +0530

    port

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div>
+      <div>Home</div>
+      <div>Project</div>
+      <div>About</div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
